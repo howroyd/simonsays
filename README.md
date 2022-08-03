831c98bc6c3dfc7d068111d977bd86eb2fb791f4
Simon Howroyd <howroydlsu@gmail.com>
2022-08-03 13:52:31 +0100

    Initial commit
    Code taken as is from https://www.dougdoug.com/twitchplays
    
    Readme added and what I think is the most appropriate license based upon how this was distributed before me

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# twitchplays
+# TwitchPlays
+This work in progress code is a tidy up of an excellent system seen on DoudDougW's Twitch channel (https://www.twitch.tv/dougdougw)
+
+They've kindly provided the code on their website (https://www.dougdoug.com/twitchplays)
+
+I was unable to find the work on github, so created a fresh repository.  If anyone knows otherwise, please let me know so I can attribute this repo to their work in the usual github way.
+
+The initial commit on `main` whows the original files as taken from the website when I started this.  The original header is at the bottom of this readme.  I've added a GNU GPL v2 as I believe this license matches the header comments and way the code has been distributed before me.  Any issues, please do let me know.
+
+---
+
+# Original file header comments
+> Written by DougDoug and DDarknut
+> 
+> Hello! This file contains the main logic to process Twitch chat and convert it to game commands.
+> The code is written in Python 3.X
+> There are 2 other files needed to run this code:
+> - `TwitchPlays_KeyCodes.py` contains the key codes and functions to press keys in-game. You should not modify this file.
+> - `TwitchPlays_Connection.py` is the code that actually connects to Twitch. You should not modify this file.
+> 
+> The source code primarily comes from:
+> - Wituz's "Twitch Plays" tutorial: http://www.wituz.com/make-your-own-twitch-plays-stream.html
+> - PythonProgramming's "Python Plays GTA V" tutorial: https://pythonprogramming.net/direct-input-game-python-plays-gta-v/
+> - DDarknut's message queue and updates to the Twitch networking code
+> 
+> Disclaimer: 
+> - This code is NOT intended to be professionally optimized or organized.
+> - We created a simple version that works well for livestreaming, and I'm sharing it for educational purposes.
---
+++ TwitchPlays_Connection.py
@@ -0,0 +1,161 @@
+# DougDoug Note: 
+# This is the code that connects to Twitch and checks for new messages.
+# You should not need to modify anything in this file, just use as is.
+# Original code by Wituz, updated by DDarknut
+
+import sys
+import socket
+import re
+import random
+import time
+
+MAX_TIME_TO_WAIT_FOR_LOGIN = 3
+
+class Twitch:
+    re_prog = None
+    sock = None
+    partial = b''
+    login_ok = False
+    channel = ''
+    login_timestamp = 0
+
+    def twitch_connect(self, channel):
+        if self.sock: self.sock.close()
+        self.sock = None
+        self.partial = b''
+        self.login_ok = False
+        self.channel = channel
+
+        # Compile regular expression
+        self.re_prog = re.compile(b'^(?::(?:([^ !\r\n]+)![^ \r\n]*|[^ \r\n]*) )?([^ \r\n]+)(?: ([^:\r\n]*))?(?: :([^\r\n]*))?\r\n', re.MULTILINE)
+
+        # Create socket
+        print('Connecting to Twitch...')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Attempt to connect socket
+        self.sock.connect(('irc.chat.twitch.tv', 6667))
+
+        # Log in anonymously
+        user = 'justinfan%i' % random.randint(10000, 99999)
+        print('Connected to Twitch. Logging in anonymously...')
+        self.sock.send(('PASS asdf\r\nNICK %s\r\n' % user).encode())
+
+        self.sock.settimeout(1.0/60.0)
+
+        self.login_timestamp = time.time()
+
+    # Attempt to reconnect after a delay
+    def reconnect(self, delay):
+        time.sleep(delay)
+        self.twitch_connect(self.channel)
+
+    # Returns a list of irc messages received
+    def receive_and_parse_data(self):
+        buffer = b''
+        while True:
+            received = b''
+            try:
+                received = self.sock.recv(4096)
+            except socket.timeout:
+                break
+            # except OSError as e:
+            #     if e.winerror == 10035:
+            #         # This "error" is expected -- we receive it if timeout is set to zero, and there is no data to read on the socket.
+            #         break
+            except Exception as e:
+                print('Unexpected connection error. Reconnecting in a second...', e)
+                self.reconnect(1)
+                return []
+            if not received:
+                print('Connection closed by Twitch. Reconnecting in 5 seconds...')
+                self.reconnect(5)
+                return []
+            buffer += received
+
+        if buffer:
+            # Prepend unparsed data from previous iterations
+            if self.partial:
+                buffer = self.partial + buffer
+                self.partial = []
+
+            # Parse irc messages
+            res = []
+            matches = list(self.re_prog.finditer(buffer))
+            for match in matches:
+                res.append({
+                    'name':     (match.group(1) or b'').decode(errors='replace'),
+                    'command':  (match.group(2) or b'').decode(errors='replace'),
+                    'params':   list(map(lambda p: p.decode(errors='replace'), (match.group(3) or b'').split(b' '))),
+                    'trailing': (match.group(4) or b'').decode(errors='replace'),
+                })
+
+            # Save any data we couldn't parse for the next iteration
+            if not matches:
+                self.partial += buffer
+            else:
+                end = matches[-1].end()
+                if end < len(buffer):
+                    self.partial = buffer[end:]
+
+                if matches[0].start() != 0:
+                    # If we get here, we might have missed a message. pepeW
+                    # ⣿⣿⣿⣿⣿⣿⣿⠿⢛⢛⡛⡻⢿⣿⣿⣿⣿⠟⠛⢛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿
+                    # ⣿⣿⣿⣿⢟⢱⡔⡝⣜⣜⢜⢜⡲⡬⡉⢕⢆⢏⢎⢇⢇⣧⡉⠿⣿⣿⣿⣿⣿⣿
+                    # ⣿⣿⡟⡱⣸⠸⢝⢅⢆⢖⣜⣲⣵⣴⣱⣈⡣⣋⢣⠭⣢⣒⣬⣕⣄⣝⡻⢿⣿⣿
+                    # ⣿⠟⡜⣎⢎⢇⢇⣵⣷⣿⣿⡿⠛⠉⠉⠛⢿⣦⢵⣷⣿⣿⣿⠟⠛⠋⠓⢲⡝⣿
+                    # ⢏⢰⢱⣞⢜⢵⣿⣿⣿⣿⣿⠁⠐⠄⠄⠄⠄⢹⣻⣿⣿⣿⠡⠄⠄⠄⠄⠄⠹⣺
+                    # ⢕⢜⢕⢕⢵⠹⢿⣿⣿⣿⣿⡀⠸⠗⣀⠄⠄⣼⣻⣿⣿⣿⡀⢾⠆⣀⠄⠄⣰⢳
+                    # ⡕⣝⢜⡕⣕⢝⣜⢙⢿⣿⣿⣷⣦⣤⣥⣤⣾⢟⠸⢿⣿⣿⣿⣦⣄⣉⣤⡴⢫⣾
+                    # ⡪⡪⣪⢪⢎⢮⢪⡪⡲⢬⢩⢩⢩⠩⢍⡪⢔⢆⢏⡒⠮⠭⡙⡙⠭⢝⣨⣶⣿⣿
+                    # ⡪⡪⡎⡮⡪⡎⡮⡪⣪⢣⢳⢱⢪⢝⢜⢜⢕⢝⢜⢎⢧⢸⢱⡹⡍⡆⢿⣿⣿⣿
+                    # ⡪⡺⡸⡪⡺⣸⠪⠚⡘⠊⠓⠕⢧⢳⢹⡸⣱⢹⡸⡱⡱⡕⡵⡱⡕⣝⠜⢿⣿⣿
+                    # ⡪⡺⡸⡪⡺⢐⢪⢑⢈⢁⢋⢊⠆⠲⠰⠬⡨⡡⣁⣉⠨⡈⡌⢥⢱⠐⢕⣼⣿⣿
+                    # ⡪⣪⢣⢫⠪⢢⢅⢥⢡⢅⢅⣑⡨⡑⠅⠕⠔⠔⠄⠤⢨⠠⡰⠠⡂⣎⣼⣿⣿⣿
+                    # ⠪⣪⡪⡣⡫⡢⡣⡣⡣⡣⡣⣣⢪⡪⡣⡣⡲⣑⡒⡎⡖⢒⣢⣥⣶⣿⣿⣿⣿⣿
+                    # ⢁⢂⠲⠬⠩⣁⣙⢊⡓⠝⠎⠮⠮⠚⢎⡣⡳⠕⡉⣬⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                    # ⢐⠐⢌⠐⠅⡂⠄⠄⢌⢉⠩⠡⡉⠍⠄⢄⠢⡁⡢⠠⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                    print('either ddarknut fucked up or twitch is bonkers, or both I mean who really knows anything at this point')
+
+            return res
+
+        return []
+
+    def twitch_receive_messages(self):
+        privmsgs = []
+        for irc_message in self.receive_and_parse_data():
+            cmd = irc_message['command']
+            if cmd == 'PRIVMSG':
+                privmsgs.append({
+                    'username': irc_message['name'],
+                    'message': irc_message['trailing'],
+                })
+            elif cmd == 'PING':
+                self.sock.send(b'PONG :tmi.twitch.tv\r\n')
+            elif cmd == '001':
+                print('Successfully logged in. Joining channel %s.' % self.channel)
+                self.sock.send(('JOIN #%s\r\n' % self.channel).encode())
+                self.login_ok = True
+            elif cmd == 'JOIN':
+                print('Successfully joined channel %s' % irc_message['params'][0])
+            elif cmd == 'NOTICE':
+                print('Server notice:', irc_message['params'], irc_message['trailing'])
+            elif cmd == '002': continue
+            elif cmd == '003': continue
+            elif cmd == '004': continue
+            elif cmd == '375': continue
+            elif cmd == '372': continue
+            elif cmd == '376': continue
+            elif cmd == '353': continue
+            elif cmd == '366': continue
+            else:
+                print('Unhandled irc message:', irc_message)
+
+        if not self.login_ok:
+            # We are still waiting for the initial login message. If we've waited longer than we should, try to reconnect.
+            if time.time() - self.login_timestamp > MAX_TIME_TO_WAIT_FOR_LOGIN:
+                print('No response from Twitch. Reconnecting...')
+                self.reconnect(0)
+                return []
+
+        return privmsgs
