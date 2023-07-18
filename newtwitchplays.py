d2fbffc8dd4030bf26f7ac2c50eca4eb022a4448
Simon Howroyd <simon.howroyd@wearable.technology>
2023-07-18 21:44:05 +0100

    Started working on tkinter gui

--- newtwitchplays.py
+++ newtwitchplays.py
@@ -1,34 +1,96 @@
 #!./.venv/bin/python3
 import concurrent.futures as cf
 import functools
-import pprint
+import queue
+import tkinter as tk
 
 import phasmoactions as pa
 import twitchactions as ta
 import twitchirc
 
+VERSION = "2.0.0"
+CHANNEL = "drgreengiant"
+
+
 def done_callback(future, command):
     print(f"Done: {command.tag=}")
 
 
-if __name__ == "__main__":
-    print("Hello World!")
+def test_callback(tag: str):
+    print(f"Test callback: {tag=}")
 
+
+def set_enabled_cb(runtime_data: tuple[str, ta.RuntimeData], enabled: tk.BooleanVar):
+    tag, runtime_data = runtime_data
+    state = enabled.get()
+    print(f"Set enabled: {state=} for {tag}")
+    runtime_data.enabled = state
+
+
+def make_gui(channel: str, action_list: ta.TwitchActionList, runtime_dict: ta.TwitchRuntimeDict) -> tk.Tk:
+    N_COLUMNS = 4
+
+    window = tk.Tk()
+    window.title(f"Twitch Plays v{VERSION} by DrGreenGiant")
+
+    tk.Label(window, text=F"Connected to #{CHANNEL}", padx=5, pady=5).grid(column=0, row=0, columnspan=N_COLUMNS, padx=5, pady=5)
+
+    column = 0
+    row = 1
+
+    for action in action_list:
+        runtime_data = runtime_dict.get(action.tag, None)
+        if runtime_data is None:
+            continue
+
+        action_frame = tk.Frame(window, borderwidth=1, relief=tk.RAISED)
+        action_frame.grid(column=column, row=row, padx=5, pady=5)
+
+        tk.Label(action_frame, text=action.tag, padx=5, pady=5).pack()
+
+        enabled = tk.BooleanVar()
+        enabled.set(runtime_data.enabled)
+        tk.Checkbutton(action_frame, text="Enabled", padx=5, pady=5, onvalue=True, offvalue=False, variable=enabled, command=functools.partial(set_enabled_cb, (action.tag, runtime_data), enabled)).pack()
+
+        tk.Button(action_frame, text="Test action", padx=5, pady=5, command=functools.partial(test_callback, action.tag)).pack()
+
+        # action_frame.pack()
+
+        column += 1
+        if column > (N_COLUMNS - 1):
+            column = 0
+            row += 1
+
+    window.update()
+    return window
+
+
+if __name__ == "__main__":
     keybinds = pa.make_default_keybinds()
     keybinds["walk_forward"] = "up"
     keybinds["crouch"] = "ctrl"
     config = pa.Config(keybinds=keybinds)
 
     phasmoActions = pa.make_action_list(config)
-    pprint.pprint(phasmoActions)
+    phasmoRuntime = ta.make_runtime_dict(phasmoActions)
+    phasmoRuntime['headbang'].enabled = False
 
     print()
 
     with (cf.ThreadPoolExecutor(max_workers=1) as executor,
-            twitchirc.TwitchIrc("drgreengiant") as irc):
+            twitchirc.TwitchIrc(CHANNEL) as irc):
+        print(f"TwitchIrc initialized to channel {CHANNEL}")
+
+        gui = make_gui(CHANNEL, phasmoActions, phasmoRuntime)
 
         while True:
-            msg = twitchirc.TwitchMessage.from_irc_message(irc.queue.get())
+            msg: twitchirc.TwitchMessage | None = None
+            try:
+                queue_msg = irc.queue.get(timeout=0.1)
+                msg = twitchirc.TwitchMessage.from_irc_message(queue_msg) if queue_msg else None
+            except queue.Empty:
+                pass
+            gui.update()
 
             if not msg:
                 continue
@@ -37,8 +99,16 @@ if __name__ == "__main__":
 
             action = ta.find_command(phasmoActions, command)
 
-            #print(f"{command=}: {action.__class__=}\n")
-
             if action is not None:
+                print(f"{command=}: {action.__class__=}")
+
+                runtime_data = phasmoRuntime.get(action.tag, None)
+
+                if not runtime_data.can_use:
+                    print(f"{action.tag=} can\'t run yet")
+                    continue
+
+                runtime_data.use_now()
+
                 future = executor.submit(action.run)
                 future.add_done_callback(functools.partial(done_callback, command=action))
--- twitchactions.py
+++ twitchactions.py
@@ -1,5 +1,7 @@
 #!./.venv/bin/python3
 import dataclasses
+import random
+import time
 
 import actions
 
@@ -27,9 +29,9 @@ def command_dict(twitchActions: TwitchActionList) -> dict[str, list[str | list[s
 def check_command(twitchAction: TwitchAction, command: str) -> str | None:
     '''Check if the command matches and return the tag'''
     if isinstance(twitchAction.command, str):
-        return twitchAction.tag if command == twitchAction.command else None
+        return twitchAction.tag if command.startswith(twitchAction.command) else None
     elif isinstance(twitchAction.command, list):
-        return twitchAction.tag if command in twitchAction.command else None
+        return twitchAction.tag if any(item.startswith(command) for item in twitchAction.command) else None
     else:
         return None
 
@@ -41,3 +43,43 @@ def find_command(twitchActions: TwitchActionList, command: str) -> TwitchAction:
         if tag is not None:
             return twitchAction
     return None
+
+
+@dataclasses.dataclass
+class RuntimeData:
+    enabled: bool = True
+    cooldown: float = 10.0
+    random_chance: int = 100
+    last_used: float = 0.0
+
+    def use_now(self) -> None:
+        '''Use the command now'''
+        self.last_used = time.time()
+
+    @property
+    def is_enabled(self) -> bool:
+        return self.enabled
+
+    @property
+    def is_ready(self) -> bool:
+        return time.time() - self.last_used > self.cooldown
+
+    @property
+    def check_random_chance(self) -> bool:
+        if 100 == self.random_chance:
+            return True
+
+        self.last_used = time.time()
+        return random.randint(0, 100) <= self.random_chance
+
+    @property
+    def can_use(self) -> bool:
+        return self.is_enabled and self.is_ready and self.check_random_chance
+
+
+TwitchRuntimeDict = dict[str, RuntimeData]
+
+
+def make_runtime_dict(action_list: TwitchActionList) -> TwitchRuntimeDict:
+    '''Make a runtime dict from a twitch action list'''
+    return {twitchAction.tag: RuntimeData() for twitchAction in action_list}
