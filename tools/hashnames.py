ef497c2096f5016cd53dfba96310e86ec5008f53
Simon Howroyd <simon.howroyd@wearable.technology>
2023-08-26 20:05:39 +0100

    Added blocklist

--- tools/hashnames.py
+++ tools/hashnames.py
@@ -2,8 +2,23 @@ import hashlib
 import pprint
 
 NAMES = [
-    "drgreengiant",
+    #    "drgreengiant",
+]
+
+OTHER = [
+    "d08ba4bb01a6bb0f41df42a6cca6544df4031b367b27d978ed1f25afac5bdf3b",
+    "6c7dae28b93893d307aa39911e2fd4aeb573be00e0eed6e52e4af46c8c1a081c",
+    "c09ee3ae3857b990a784a192b4260c9e31f38dc836ccc5fa10f080ddbc375612",
 ]
 
 if __name__ == "__main__":
-    pprint.pprint([hashlib.sha256(name.strip().lower().encode("utf-8")).hexdigest() for name in NAMES])
+    names = set(NAMES)
+
+    blocked = [hashlib.sha256(name.strip().lower().encode("utf-8")).hexdigest() for name in names]
+
+    blocked.extend(OTHER)
+
+    pprint.pprint(blocked)
+
+    with open("blocklist", "w") as f:
+        f.write("\n".join(blocked))
